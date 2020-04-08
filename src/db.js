e22b3fff99c7bc92eac12d5ad6a6725360357b22
Brian Barrow <brian.barrow@chghealthcare.com>
2020-04-07 20:44:34 -0600

    updated config

--- src/db.js
+++ src/db.js
@@ -4,14 +4,9 @@ import 'firebase/firestore'
 // Get a Firestore instance
 export const db = firebase
   .initializeApp({ 
-    apiKey: "AIzaSyCsI11lGPxL4Ii5hizh4Ek7hXKCHsv0muE",
     authDomain: "gtg-app-2ca0c.firebaseapp.com",
     databaseURL: "https://gtg-app-2ca0c.firebaseio.com",
     projectId: "gtg-app-2ca0c",
-    storageBucket: "gtg-app-2ca0c.appspot.com",
-    messagingSenderId: "963454521224",
-    appId: "1:963454521224:web:391adee0807bb55e3c743d",
-    measurementId: "G-PZQKV8XNG2"
   })
   .firestore()
 
